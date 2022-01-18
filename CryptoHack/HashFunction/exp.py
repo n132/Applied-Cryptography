2755f7ea35f231aec35d3502eb90def55a7ea6f8
n132 <xm2146@nyu.edu>
2022-01-18 12:17:43 -0500

    hash collision

--- CryptoHack/HashFunction/exp.py
+++ CryptoHack/HashFunction/exp.py
@@ -1,3 +1,7 @@
+import hashlib
+import json
+from pwn import *
+context.log_level='debug'
 def birthday2():
     i =1 
     while(1):
@@ -18,4 +22,16 @@ def birthday1():
             print(i)
             break
         i+=1
-birthday1()
+def Collider():
+    with open("0a959025-1.png",'rb') as f:
+        d1 = f.read()
+    with open("0a959025-2.png",'rb') as f:
+        d2 = f.read()
+    data = {}
+    data['document'] = d1.hex()
+    p=remote("socket.cryptohack.org",13389)
+    p.sendafter("\n",json.dumps(data))
+    p.interactive()
+    data = {}
+    data['document'] = d1.hex()
+Collider()
